30924eb54c8e0e061a7f1ff80321e26cceb5b70f
gesslar <karahd@gmail.com>
2024-11-20 01:06:12 -0500

    trying again but now with zest

--- src/action/action.js
+++ src/action/action.js
@@ -1,43 +1,9 @@
-const {Gludoc, Environment} = require('../core/core')
+const core = require('@actions/core');
+const { Gludoc, Environment } = require('../core/core')
 const fs = require('fs');
-const yargs = require('yargs');
 
-const argv = yargs
-  .option('source', {
-    alias: 's',
-    type: 'string',
-    describe: 'Source directory root to process',
-    demandOption: false, // Optional
-  })
-  .option('destination', {
-    alias: 'd',
-    type: 'string',
-    describe: 'Directory for the output markdown',
-    demandOption: true, // Makes this option required
-  })
-  .option('debug', {
-    type: 'boolean',
-    describe: 'Enable debug mode',
-    default: false,
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
-
-(async () => {
+async function run() {
   try {
-    const config = {
-    ...{
-      owner: "gludoc",
-      env: Environment.CLI,
-      source: "./",
-      destination: "dist/",
-      debug: false,
-    },
-    ...argv
-    }
-
-    const gludoc = new Gludoc(config);
 
     /**
      * Find all files in the source path, recursively.
@@ -49,7 +15,7 @@ const argv = yargs
       const result = []
       const sourceFiles = fs.readdirSync(path, { withFileTypes: true })
       for (const file of sourceFiles) {
-        if(file.isDirectory()) {
+        if (file.isDirectory()) {
           result.push(...findFiles(`${path}/${file.name}`))
         }
         if (file.isFile() && file.name.endsWith('.lua')) {
@@ -59,14 +25,27 @@ const argv = yargs
       return result
     }
 
-    const files = findFiles(config.source)
+    const source = core.getInput('source');
+    const destination = core.getInput('destination');
+    const debug = core.getInput('debug');
+
+    const config = {
+      owner: "gludoc",
+      env: Environment.ACTION,
+      source,
+      destination,
+      debug,
+    }
+
+    const gludoc = new Gludoc(config);
+    const files = findFiles(config.source);
     const result = await gludoc.processFiles(files);
     const successes = result.filter(({ error }) => !error);
     const errors = result.filter(({ error }) => error);
 
     const logger = gludoc.logger
     if (errors.length) {
-      if(errors.length == files.length) {
+      if (errors.length == files.length) {
         logger.warn('No documentation generated.');
         return;
       } else {
@@ -79,7 +58,10 @@ const argv = yargs
         gludoc.logger.info('No files found to generate documentation.');
       }
     }
-  } catch (e) {
-    console.error(`Error generating documentation: ${e.message}`);
+
+  } catch (error) {
+    core.setFailed(`Action failed with error: ${error.message}`);
   }
-})();
+}
+
+run();
